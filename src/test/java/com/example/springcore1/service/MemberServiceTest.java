3572fddfabb628448ec3e0fc8e33095337a2e7f7
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-12-06 19:11:11 +0900

    add MemberService Test코드 작성

---
+++ src/test/java/com/example/springcore1/service/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.springcore1.service;
+
+import com.example.springcore1.domain.Grade;
+import com.example.springcore1.domain.Member;
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+@DisplayName("MemberService 클래스의")
+class MemberServiceTest {
+
+    private MemberService memberService = new MemberServiceImpl();
+
+    @DisplayName("join 메소드는")
+    @Nested
+    class Describe_join {
+
+        @DisplayName("만약 아이디기 중복되지 않은 멤버를 저장할 경우")
+        @Nested
+        class Context_with_saved_valid_member {
+
+            @DisplayName("회원 정보를 저장한다.")
+            @Test
+            void join() {
+                //given
+                Member memberA = new Member(1L, "memberA", Grade.VIP);
+
+                //when
+                memberService.join(memberA);
+                Member findMember = memberService.findMember(memberA.getId());
+
+                //then
+                assertThat(findMember).isEqualTo(findMember);
+            }
+        }
+    }
+}
